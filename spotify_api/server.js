4f878f14481971fe7ec4bdd819c0651d83e4f790
RathGate <mariannecorbel@hotmail.fr>
2025-01-11 00:40:20 +0100

    added server logs

--- spotify_api/server.js
+++ spotify_api/server.js
@@ -5,4 +5,10 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     log.info(`Server is running on port ${PORT}`);
+
+    console.log(`\nServer is running on port ${PORT} !`)
+    console.log("----")
+    console.log(`Link : http://localhost:${PORT}`)
+    console.log("Documentation : http://localhost:3000/api-docs")
+    console.log("----\n")
 });
